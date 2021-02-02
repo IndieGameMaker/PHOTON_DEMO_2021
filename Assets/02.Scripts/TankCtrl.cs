b8f28df442fca581a968975dcf935a0e00b208f0
Lee JaeHyun <myleje@gmail.com>
2021-02-02 15:48:15 +0900

    .

--- Assets/02.Scripts/TankCtrl.cs
+++ Assets/02.Scripts/TankCtrl.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class TankCtrl : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    public float moveSpeed = 10.0f;
+    public float turnSpeed = 100.0f;
+
+    private Transform tr;
+
     void Start()
     {
-        
+        tr = GetComponent<Transform>();        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float v = Input.GetAxis("Vertical");   // Up, Down Arrow , W, S [-1.0f ~ 0.0f ~ 1.0f]
+        float h = Input.GetAxis("Horizontal"); // Left, Right Arrow, A, D [-1.0f ~ 0.0f ~ 1.0f]
+
+        tr.Translate(Vector3.forward * Time.deltaTime * moveSpeed * v);
+        tr.Rotate(Vector3.up * Time.deltaTime * turnSpeed * h);        
     }
 }
